69374e59617e7e134f4b418a22afdab873f5d161
Pierre Chalamet <pierre@chalamet.net>
2023-09-09 21:36:38 +0200

    code cleanup

--- Terrabuild/Configuration.fs
+++ Terrabuild/Configuration.fs
@@ -4,52 +4,19 @@ open Helpers.Collections
 open Helpers
 open System.IO
 
-(*
-   █████████     ███████    ██████   ██████ ██████   ██████    ███████    ██████   █████
-  ███░░░░░███  ███░░░░░███ ░░██████ ██████ ░░██████ ██████   ███░░░░░███ ░░██████ ░░███
- ███     ░░░  ███     ░░███ ░███░█████░███  ░███░█████░███  ███     ░░███ ░███░███ ░███
-░███         ░███      ░███ ░███░░███ ░███  ░███░░███ ░███ ░███      ░███ ░███░░███░███
-░███         ░███      ░███ ░███ ░░░  ░███  ░███ ░░░  ░███ ░███      ░███ ░███ ░░██████
-░░███     ███░░███     ███  ░███      ░███  ░███      ░███ ░░███     ███  ░███  ░░█████
- ░░█████████  ░░░███████░   █████     █████ █████     █████ ░░░███████░   █████  ░░█████
-  ░░░░░░░░░     ░░░░░░░    ░░░░░     ░░░░░ ░░░░░     ░░░░░    ░░░░░░░    ░░░░░    ░░░░░
-*)
-
 type Dependencies = list<string>
 
 type Outputs = list<string>
 
+type ProjectTargets = map<string, Dependencies>
 
-(*
- ███████████  ███████████      ███████          █████ ██████████   █████████  ███████████
-░░███░░░░░███░░███░░░░░███   ███░░░░░███       ░░███ ░░███░░░░░█  ███░░░░░███░█░░░███░░░█
- ░███    ░███ ░███    ░███  ███     ░░███       ░███  ░███  █ ░  ███     ░░░ ░   ░███  ░
- ░██████████  ░██████████  ░███      ░███       ░███  ░██████   ░███             ░███
- ░███░░░░░░   ░███░░░░░███ ░███      ░███       ░███  ░███░░█   ░███             ░███
- ░███         ░███    ░███ ░░███     ███  ███   ░███  ░███ ░   █░░███     ███    ░███
- █████        █████   █████ ░░░███████░  ░░████████   ██████████ ░░█████████     █████
-░░░░░        ░░░░░   ░░░░░    ░░░░░░░     ░░░░░░░░   ░░░░░░░░░░   ░░░░░░░░░     ░░░░░
-*)
-
-type ProjectTargets = map<string, list<string>>
+type DependsOns = list<string>
 
 type ProjectConfiguration = {
     [<YamlField("dependencies")>] Dependencies: Dependencies option
     [<YamlField("outputs")>] Outputs: Outputs option
     [<YamlField("targets")>] Targets: ProjectTargets option
 }
-
-
-(*
- ███████████  █████  █████ █████ █████       ██████████
-░░███░░░░░███░░███  ░░███ ░░███ ░░███       ░░███░░░░███
- ░███    ░███ ░███   ░███  ░███  ░███        ░███   ░░███
- ░██████████  ░███   ░███  ░███  ░███        ░███    ░███
- ░███░░░░░███ ░███   ░███  ░███  ░███        ░███    ░███
- ░███    ░███ ░███   ░███  ░███  ░███      █ ░███    ███
- ███████████  ░░████████   █████ ███████████ ██████████
-░░░░░░░░░░░    ░░░░░░░░   ░░░░░ ░░░░░░░░░░░ ░░░░░░░░░░
-*)
 
 type StoreType =
     | [<YamlValue("local")>] Local
@@ -60,7 +27,7 @@ type Store = {
 }
 
 type BuildTarget = {
-    [<YamlField("depends-on")>] DependsOn: list<string> option
+    [<YamlField("depends-on")>] DependsOn: DependsOns option
 }
 
 type BuildTargets = map<string, BuildTarget>
@@ -76,11 +43,11 @@ type BuildConfiguration = {
 
 
 
+
 type GlobalConfiguration = {
     Build: BuildConfiguration
     Projects: map<string, ProjectConfiguration>
 }
-
 
 let read workspaceDirectory =
     let buildFile = Path.Combine(workspaceDirectory, "BUILD")
@@ -93,11 +60,14 @@ let read workspaceDirectory =
             let dependencyDirectory = Path.Combine(cwd, dependency)
             let dependencyId = Path.GetRelativePath(workspaceDirectory, dependencyDirectory)
 
+            // process only unknown dependency
             if projects |> Map.containsKey dependency |> not then
                 let dependencyFile = Path.Combine(dependencyDirectory, "PROJECT")
                 let dependencyConfig =
-                    if File.Exists(dependencyFile) then Json.DeserializeFile<ProjectConfiguration> dependencyFile
-                    else { Dependencies = None; Outputs = None; Targets = None }
+                    if File.Exists(dependencyFile) then
+                        Json.DeserializeFile<ProjectConfiguration> dependencyFile
+                    else
+                        { Dependencies = None; Outputs = None; Targets = None }
                 projects <- projects |> Map.add dependencyId dependencyConfig
 
                 let dependencyRelativePathes = dependencyConfig.Dependencies |> Option.defaultValue List.empty
