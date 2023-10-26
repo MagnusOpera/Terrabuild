888746a5e77d78d7cf3cf4b98bb338d68165d5e8
Pierre Chalamet <pierre@chalamet.net>
2023-10-26 14:55:04 +0200

    cleanup code

--- Terrabuild/Helpers/Terminal.fs
+++ Terrabuild/Helpers/Terminal.fs
@@ -1,14 +1,14 @@
 module Terminal
 open System
 
+let flush () =
+    Console.Out.Flush()
+
 let write (str: string) =
     Console.Out.Write(str)
 
 let writeLine (str: string) =
     Console.Out.WriteLine(str)
-
-let flush () =
-    Console.Out.Flush()
 
 let hideCursor() =
     Ansi.Styles.cursorHide |> write |> flush
