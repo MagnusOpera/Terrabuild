bd5c1be0b2a0111d19d7d2f2dd967551d66ec08c
Pierre Chalamet <pierre@chalamet.net>
2024-03-06 22:36:39 +0100

    restore before publish

--- src/Terrabuild.Extensions/Dotnet.fs
+++ src/Terrabuild.Extensions/Dotnet.fs
@@ -56,7 +56,7 @@ type Dotnet() =
         let projectFile = context.Properties["projectfile"]
         let configuration = configuration |> Option.defaultValue "Debug"
 
-        [ Action.Build "dotnet" $"restore {projectFile} --no-dependencies" Cacheability.Always
+        [ Action.Build "dotnet" $"restore {projectFile} --no-dependencies" Cacheability.Local
           Action.Build "dotnet" $"build {projectFile} -m:1 --no-dependencies --no-restore --configuration {configuration}" Cacheability.Always ]
 
 
@@ -89,7 +89,8 @@ type Dotnet() =
             match single with
             | Some true -> " --self-contained"
             | _ -> ""
-        [ Action.Build "dotnet" $"publish {projectFile} --configuration {configuration}{runtime}{trim}{single}" Cacheability.Always ]
+        [ Action.Build "dotnet" $"restore {projectFile}" Cacheability.Always
+          Action.Build "dotnet" $"publish {projectFile} --configuration {configuration}{runtime}{trim}{single}" Cacheability.Always ]
 
 
     static member Restore (context: ActionContext) =
