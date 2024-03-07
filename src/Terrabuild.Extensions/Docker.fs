c0b339a0a465fb533c6387838579f81e64fc3844
Pierre Chalamet <pierre@magnusopera.io>
2024-03-07 11:05:16 +0100

    fix branch or tag for docker

--- src/Terrabuild.Extensions/Docker.fs
+++ src/Terrabuild.Extensions/Docker.fs
@@ -23,12 +23,12 @@ type Docker() =
 
 
     static member Push (context: ActionContext) (image: string) =
+        let branchOrTag = context.BranchOrTag.Replace("/", "-")
         if context.CI then
-            let retagArgs = $"buildx imagetools create -t {image}:{context.BranchOrTag} {image}:{context.NodeHash}"
+            let retagArgs = $"buildx imagetools create -t {image}:{branchOrTag} {image}:{context.NodeHash}"
             scope Cacheability.Remote
             |> andThen "docker" retagArgs
         else
-            let tagArgs = $"tag {image}:{context.NodeHash} {image}:{context.BranchOrTag}"
+            let tagArgs = $"tag {image}:{context.NodeHash} {image}:{branchOrTag}"
             scope Cacheability.Local
             |> andThen "docker" tagArgs
-
