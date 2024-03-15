20e5ae50bbc1e692e1a85c99b2638464a03ad6f0
Pierre Chalamet <pierre@chalamet.net>
2024-03-15 22:35:40 +0100

    code cleanup

--- src/DocGen/Program.fs
+++ src/DocGen/Program.fs
@@ -39,13 +39,8 @@ let (|Extension|Command|) s =
     | _ -> failwith $"Unknown member kind: {s}"
 
 
-[<EntryPoint>]
-let main args =
-    let doc = XmlDoc.load args[0]
-    if doc.Assembly.Name <> "Terrabuild.Extensions" then failwith "Expecting documentation for Terrabuild.Extensions"
 
-    let members = doc.Members |> Option.ofObj |> Option.defaultValue Array.empty
-
+let buildExtensions (members: XmlDoc.Member seq) =
     // first find extensions
     let extensions =
         members
@@ -83,86 +78,134 @@ let main args =
                 ext.Commands <- ext.Commands @ [cmd]
         | _ -> ())
 
+    extensions
+    |> Map.iter (fun _ ext -> ext.Commands <- ext.Commands |> List.sortBy (fun x -> x.Weight))
+
+    extensions
+
+
+
+let writeCommand extensionDir (command: Command) (extension: Extension) =
+    match command.Name with
+    | "__init__" -> ()
+    | _ ->
+        let commandFile = Path.Combine(extensionDir, $"{command.Name}.md")
+        let commandContent = [
+            "---"
+            $"title: \"{command.Name}\""
+            if command.Weight |> Option.isSome then $"weight: {command.Weight.Value}"
+            "---"
+            ""
+            command.Summary
+            "```"
+            match command.Parameters with
+            | [] -> $"@{extension.Name} {command.Name}"
+            | prms ->
+                $"@{extension.Name} {command.Name} {{"
+                for prm in prms do
+                    $"    {prm.Name} {prm.Demo}"
+                "}"
+            "```"
+            $"## Argument Reference"
+            match command.Parameters with
+            | [] -> "This command does not accept arguments."
+            | prms ->
+                "The following arguments are supported:"
+                for prm in prms do
+                    let required = if prm.Required then "Required" else "Optional"
+                    $"* `{prm.Name}` - ({required}) {prm.Summary}"
+        ]
+        File.WriteAllLines(commandFile, commandContent)
+
+
+
+let writeExtension extensionDir (extension: Extension) =
+    // generate extension index
+    let extensionFile = Path.Combine(extensionDir, "_index.md")
+    let extensionContent = [
+        "---"
+        $"title: \"{extension.Name}\""
+        "---"
+        ""
+        extension.Summary
+        ""
+        "## Available Commands"
+        match extension.Commands with
+        | [] -> "This extension has no commands."
+        | _ ->
+            "| Command | Description |"
+            "|---------|-------------|"
+            for cmd in extension.Commands do
+                match cmd.Name with
+                | "__init__" -> ()
+                | _ ->
+                    $"| [{cmd.Name}](/docs/extensions/{extension.Name}/{cmd.Name}) | {cmd.Title |> Option.defaultValue cmd.Summary} |"
+
+            match extension.Commands |> List.tryFind (fun cmd -> cmd.Name = "__init__") with
+            | Some init ->
+                ""
+                $"## Project Initializer"
+                "```"
+                $"configuration @{extension.Name}"
+                "```"
+                init.Summary
+                for prm in init.Parameters do $"* `{prm.Name}` - {prm.Summary}"
+            | _ -> ()
+    ]
+    File.WriteAllLines(extensionFile, extensionContent)
+
+
+[<EntryPoint>]
+let main args =
+    let doc = XmlDoc.load args[0]
+    let outputDir = args[1]
+    if doc.Assembly.Name <> "Terrabuild.Extensions" then failwith "Expecting documentation for Terrabuild.Extensions"
+
+    let members = doc.Members |> Option.ofObj |> Option.defaultValue Array.empty
+    let extensions = buildExtensions members
 
     // generate files
-    let outputDir = args[1]
-
-
-    for extension in extensions do
-        let extension = extension.Value
-        printfn $"Generating docs for {extension.Name}"
-
-        // generate extension index
+    printfn "Generating docs"
+    for (KeyValue(_, extension)) in extensions do
         let extensionDir = Path.Combine(outputDir, extension.Name)
         if Directory.Exists extensionDir |> not then Directory.CreateDirectory extensionDir |> ignore
 
-        let extensionFile = Path.Combine(extensionDir, "_index.md")
-        let extensionContent = [
-            "---"
-            $"title: \"{extension.Name}\""
-            "---"
-            ""
-            extension.Summary
-            ""
-            "## Available Commands"
-            match extension.Commands with
-            | [] -> "This extension has no commands."
-            | _ ->
-                "| Command | Description |"
-                "|---------|-------------|"
-                for cmd in extension.Commands |> List.sortBy (fun x -> x.Weight) do
-                    match cmd.Name with
-                    | "__init__" -> ()
-                    | _ ->
-                        $"| [{cmd.Name}](/docs/extensions/{extension.Name}/{cmd.Name}) | {cmd.Title |> Option.defaultValue cmd.Summary} |"
-
-                match extension.Commands |> List.tryFind (fun cmd -> cmd.Name = "__init__") with
-                | Some init ->
-                    ""
-                    $"## Project Initializer"
-                    "```"
-                    $"configuration @{extension.Name}"
-                    "```"
-                    init.Summary
-                    for prm in init.Parameters do
-                        $"* `{prm.Name}` - {prm.Summary}"
-                | _ -> ()
-        ]
-        File.WriteAllLines(extensionFile, extensionContent)
+        printfn $"  {extension.Name}"
+        writeExtension extensionDir extension
 
         // generate extension commands
         for cmd in extension.Commands do
-            match cmd.Name with
-            | "__init__" -> ()
-            | _ ->
-                let commandFile = Path.Combine(extensionDir, $"{cmd.Name}.md")
-                let commandContent = [
-                    "---"
-                    $"title: \"{cmd.Name}\""
-                    if cmd.Weight |> Option.isSome then $"weight: {cmd.Weight.Value}"
-                    "---"
-                    ""
-                    cmd.Summary
-                    "```"
-                    match cmd.Parameters with
-                    | [] -> $"@{extension.Name} {cmd.Name}"
-                    | prms ->
-                        $"@{extension.Name} {cmd.Name} {{"
-                        for prm in prms do
-                            $"    {prm.Name} {prm.Demo}"
-                        "}"
-                    "```"
-                    $"## Argument Reference"
-                    match cmd.Parameters with
-                    | [] -> "This command does not accept arguments."
-                    | prms ->
-                        "The following arguments are supported:"
-                        for prm in prms do
-                            let required = if prm.Required then "Mandatory" else "Optional"
-                            $"* `{prm.Name}` - ({required}) {prm.Summary}"
-                ]
-                File.WriteAllLines(commandFile, commandContent)
+            writeCommand extensionDir cmd extension
 
+    // cleanup
+    printfn "Cleaning output"
+    let genExtensions = extensions.Keys |> Set.ofSeq
+    let folders =
+        Directory.EnumerateDirectories(outputDir)
+        |> Seq.map Path.GetFileName
+        |> Set.ofSeq
+    let removeFolders = folders - genExtensions
+    for folder in removeFolders do
+        let folder = Path.Combine(outputDir, folder)
+        printfn $"  Removing {folder}"
+        Directory.Delete(folder, true)
+
+    for (KeyValue(_, extension)) in extensions do
+        let extensionDir = Path.Combine(outputDir, extension.Name)
+        let genCommands =
+            extension.Commands
+            |> List.map (fun cmd -> $"{cmd.Name}.md")
+            |> Set.ofSeq
+            |> Set.add "_index.md"
+        let commands =
+            Directory.EnumerateFiles(extensionDir)
+            |> Seq.map Path.GetFileName
+            |> Set.ofSeq
+        let removeCommands = commands - genCommands
+        for command in removeCommands do
+            let file = Path.Combine(extensionDir, command)
+            printfn $"  Removing {file}"
+            File.Delete(file)
 
     printfn "Done"
     0
