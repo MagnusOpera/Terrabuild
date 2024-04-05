13e3a7c3e533a4cf723bc2d8c0a23bcb7bde26e5
Pierre Chalamet <pierre@magnusopera.io>
2024-04-05 14:30:59 +0200

    rework launch dir handling

--- src/Terrabuild/Program.fs
+++ src/Terrabuild/Program.fs
@@ -38,6 +38,8 @@ type TerrabuildExiter() =
 
             exit (int errorCode)
 
+let launchDir = Environment.CurrentDirectory
+
 let processCommandLine () =
     let errorHandler = TerrabuildExiter()
     let parser = ArgumentParser.Create<CLI.TerrabuildArgs>(programName = "terrabuild", errorHandler = errorHandler)
@@ -45,7 +47,6 @@ let processCommandLine () =
     let debug = result.Contains(TerrabuildArgs.Debug)
     let whatIf = result.Contains(TerrabuildArgs.WhatIf)
 
-    let launchDir = Environment.CurrentDirectory
     let logFile name = IO.combinePath launchDir $"terrabuild-debug.{name}"
 
     if debug then
@@ -56,65 +57,57 @@ let processCommandLine () =
                 .CreateLogger()
 
     let runTarget wsDir target environment labels variables (options: Configuration.Options) =
-        let runTarget () =
-            try
-                if options.Debug then
-                    let jsonOptions = Json.Serialize options
-                    jsonOptions |> IO.writeTextFile (logFile "options.json")
-
-                $"{Ansi.Emojis.box} Reading configuration" |> Terminal.writeLine
-                let config = Configuration.read options environment labels variables
-
-                if options.Debug then
-                    let jsonConfig = Json.Serialize config
-                    jsonConfig |> IO.writeTextFile (logFile "config.json")
-
-                $"{Ansi.Emojis.popcorn} Constructing graph for {config.Environment}" |> Terminal.writeLine
-                let graph = Graph.buildGraph config target
-
-                if options.Debug then
-                    let jsonGraph = Json.Serialize graph
-                    jsonGraph |> IO.writeTextFile (logFile "graph.json")
-                    let mermaid = Graph.graph graph |> String.join "\n"
-                    mermaid |> IO.writeTextFile (logFile "graph.mermaid")
-
-                let cache = Cache.Cache(config.Storage) :> Cache.ICache
-                let buildGraph = Graph.optimize config graph cache options
-                if options.Debug then
-                    let jsonBuildGraph = Json.Serialize buildGraph
-                    jsonBuildGraph |> IO.writeTextFile (logFile "buildgraph.json")
-                    let mermaid = Graph.graph buildGraph |> String.join "\n"
-                    mermaid |> IO.writeTextFile (logFile "buildgraph.mermaid")
-
-                if options.WhatIf then 0
-                else
-                    let buildNotification = Notification.BuildNotification() :> Build.IBuildNotification
-                    let build = Build.run config buildGraph cache buildNotification options
-                    buildNotification.WaitCompletion()
-
-                    if options.Debug then
-                        let jsonBuild = Json.Serialize build
-                        jsonBuild |> IO.writeTextFile (logFile "build.json")
-
-                    if build.Status = Build.BuildStatus.Success then 0
-                    else 5
-
-            with
-                | :? Configuration.ConfigException as ex ->
-                    Log.Fatal("Failed with {Exception}", ex)
-                    let reason = 
-                        if options.Debug then ex.ToString()
-                        else dumpUnknownException ex |> String.join "\n   "
-                    $"{Ansi.Emojis.explosion} {reason}" |> Terminal.writeLine
-                    5
-
-        // WARNING: current process directory is changed to root of workspace
         try
             Environment.CurrentDirectory <- wsDir
-            runTarget()
-        finally
-            Environment.CurrentDirectory <- launchDir
 
+            if options.Debug then
+                let jsonOptions = Json.Serialize options
+                jsonOptions |> IO.writeTextFile (logFile "options.json")
+
+            $"{Ansi.Emojis.box} Reading configuration" |> Terminal.writeLine
+            let config = Configuration.read options environment labels variables
+
+            if options.Debug then
+                let jsonConfig = Json.Serialize config
+                jsonConfig |> IO.writeTextFile (logFile "config.json")
+
+            $"{Ansi.Emojis.popcorn} Constructing graph for {config.Environment}" |> Terminal.writeLine
+            let graph = Graph.buildGraph config target
+
+            if options.Debug then
+                let jsonGraph = Json.Serialize graph
+                jsonGraph |> IO.writeTextFile (logFile "graph.json")
+                let mermaid = Graph.graph graph |> String.join "\n"
+                mermaid |> IO.writeTextFile (logFile "graph.mermaid")
+
+            let cache = Cache.Cache(config.Storage) :> Cache.ICache
+            let buildGraph = Graph.optimize config graph cache options
+            if options.Debug then
+                let jsonBuildGraph = Json.Serialize buildGraph
+                jsonBuildGraph |> IO.writeTextFile (logFile "buildgraph.json")
+                let mermaid = Graph.graph buildGraph |> String.join "\n"
+                mermaid |> IO.writeTextFile (logFile "buildgraph.mermaid")
+
+            if options.WhatIf then 0
+            else
+                let buildNotification = Notification.BuildNotification() :> Build.IBuildNotification
+                let build = Build.run config buildGraph cache buildNotification options
+                buildNotification.WaitCompletion()
+
+                if options.Debug then
+                    let jsonBuild = Json.Serialize build
+                    jsonBuild |> IO.writeTextFile (logFile "build.json")
+
+                if build.Status = Build.BuildStatus.Success then 0
+                else 5
+        with
+            | :? Configuration.ConfigException as ex ->
+                Log.Fatal("Failed with {Exception}", ex)
+                let reason = 
+                    if options.Debug then ex.ToString()
+                    else dumpUnknownException ex |> String.join "\n   "
+                $"{Ansi.Emojis.explosion} {reason}" |> Terminal.writeLine
+                5
 
 
     let scaffold (scaffoldArgs: ParseResults<ScaffoldArgs>) =
@@ -181,10 +174,14 @@ let main _ =
         Terminal.hideCursor()
         Console.CancelKeyPress.Add (fun _ -> $"{Ansi.Emojis.bolt} Aborted{Ansi.Styles.cursorShow}" |> Terminal.writeLine)
         let ret = processCommandLine()
+
+        Environment.CurrentDirectory <- launchDir
         Terminal.showCursor()
         ret
     with
         ex ->
             $"{Ansi.Emojis.bomb} Failed with error\n{ex}{Ansi.Styles.cursorShow}" |> Terminal.writeLine
+
+            Environment.CurrentDirectory <- launchDir
             Terminal.showCursor()
             5
