67df2f96762a87520874ec73890d8d9a84e0b36e
Pierre Chalamet <pierre@magnusopera.io>
2024-04-08 17:31:00 +0200

    code cleanup

--- src/Terrabuild.Extensions/Factory.fs
+++ src/Terrabuild.Extensions/Factory.fs
@@ -4,11 +4,11 @@ module Terrabuild.Extensions.Factory
 // do not forget to add reference when adding new implementation
 let systemScripts =
     Map [
-        "@docker", typeof<Terrabuild.Extensions.Docker>
-        "@dotnet", typeof<Terrabuild.Extensions.Dotnet>
-        "@make", typeof<Terrabuild.Extensions.Make>
-        "@npm", typeof<Terrabuild.Extensions.Npm>
-        "@null", typeof<Terrabuild.Extensions.Null>
-        "@shell", typeof<Terrabuild.Extensions.Shell>
-        "@terraform", typeof<Terrabuild.Extensions.Terraform>
+        "@docker", typeof<Docker>
+        "@dotnet", typeof<Dotnet>
+        "@make", typeof<Make>
+        "@npm", typeof<Npm>
+        "@null", typeof<Null>
+        "@shell", typeof<Shell>
+        "@terraform", typeof<Terraform>
     ]
