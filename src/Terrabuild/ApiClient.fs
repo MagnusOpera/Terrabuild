1f1440209442b241535a03c80df5b7b8f3dcb636
Pierre Chalamet <pierre@magnusopera.io>
2024-04-22 11:30:54 +0200

    refactor api access

---
+++ src/Terrabuild/ApiClient.fs
@@ -0,0 +1,53 @@
+module ApiClient
+open System
+open FSharp.Data
+
+
+let private apiUrl =
+    let baseUrl = DotNetEnv.Env.GetString("TERRABUILD_API_URL", "https://api.terrabuild.io")
+    Uri(baseUrl)
+
+let inline private request<'req, 'resp> method (path: string) (request: 'req) =
+    let url = Uri(apiUrl, path).ToString()
+    let body =
+        if typeof<'req> <> typeof<Unit> then request |> FSharpJson.Serialize |> TextRequest |> Some
+        else None
+
+    let headers = [
+        HttpRequestHeaders.Accept HttpContentTypes.Json
+        HttpRequestHeaders.ContentType HttpContentTypes.Json ]
+    let response = Http.RequestString(url = url, headers = headers, ?body = body, httpMethod = method)
+
+    if typeof<'resp> <> typeof<Unit> then response |> FSharpJson.Deserialize<'resp>
+    else Unchecked.defaultof<'resp>
+
+let inline private options<'req, 'resp> = request<'req, 'resp> HttpMethod.Options
+let inline private get<'req, 'resp> = request<'req, 'resp> HttpMethod.Get
+let inline private post<'req, 'resp> = request<'req, 'resp> HttpMethod.Post
+
+
+module Auth =
+    [<RequireQualifiedAccess>]
+    type AuthenticateInput = {
+        Token: string
+    }
+
+    [<RequireQualifiedAccess>]
+    type LoginSpaceInput = {
+        Space: string
+        Token: string
+    }
+
+    [<RequireQualifiedAccess>]
+    type LoginSpaceOutput = {
+        AccessToken: string
+    }
+
+    let authenticate token =
+        { AuthenticateInput.Token = token }
+        |> options<AuthenticateInput, Unit> "/auth" 
+
+    let loginSpace space token =
+        { LoginSpaceInput.Space = space
+          LoginSpaceInput.Token = token }
+        |> post<LoginSpaceInput, LoginSpaceOutput> "/auth"
