bd852fb7a51d1d00ea247783ae8fe8244f24e5d0
Pierre Chalamet <pierre@magnusopera.io>
2024-04-23 18:33:40 +0200

    fix again branch or tag

--- src/Terrabuild/Helpers/Git.fs
+++ src/Terrabuild/Helpers/Git.fs
@@ -6,9 +6,10 @@ let getHeadCommit (dir: string) =
     | _ -> failwith "Failed to get head commit"
 
 let getBranchOrTag (dir: string) =
-    match Exec.execCaptureOutput dir "git" "describe --exact-match --tags" with
+    // https://stackoverflow.com/questions/18659425/get-git-current-branch-tag-name
+    match Exec.execCaptureOutput dir "git" "symbolic-ref -q --short HEAD" with
     | Exec.Success (output, _) -> output |> String.firstLine
     | _ -> 
-        match Exec.execCaptureOutput dir "git" "rev-parse --abbrev-ref HEAD" with
+        match Exec.execCaptureOutput dir "git" "describe --tags --exact-match" with
         | Exec.Success (output, _) -> output |> String.firstLine
         | _ -> failwith "Failed to get branch or tag"
