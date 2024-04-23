0380572c9791457ad0b3150832df03acdb9a3a98
Pierre Chalamet <pierre@magnusopera.io>
2024-04-23 17:17:05 +0200

    add branch/commit for local

--- src/Terrabuild/Helpers/Git.fs
+++ src/Terrabuild/Helpers/Git.fs
@@ -3,4 +3,9 @@ module Git
 let getHeadCommit (dir: string) =
     match Exec.execCaptureOutput dir "git" "rev-parse HEAD" with
     | Exec.Success (output, _) -> output |> String.firstLine
-    | _ -> failwith "Failed to get listing"
+    | _ -> failwith "Failed to get head commit"
+
+let getBranch (dir: string) =
+    match Exec.execCaptureOutput dir "git" "rev-parse --abbrev-ref HEAD" with
+    | Exec.Success (output, _) -> output |> String.firstLine
+    | _ -> failwith "Failed to get branch"
--- src/Terrabuild/SourceControls/Local.fs
+++ src/Terrabuild/SourceControls/Local.fs
@@ -3,9 +3,13 @@ namespace SourceControls
 type Local() =
     inherit Contracts.SourceControl()
 
-    override _.HeadCommit = "local"
+    override _.HeadCommit =
+        // NOTE: assuming current directory is a git repository
+        System.Environment.CurrentDirectory |> Git.getHeadCommit
 
-    override _.BranchOrTag = "local"
+    override _.BranchOrTag =
+        // NOTE: assuming current directory is a git repository
+        System.Environment.CurrentDirectory |> Git.getBranch
 
     override _.CI = false
 
