48177e8cf6cdafa357ccc3814e68a8766eea6986
Pierre Chalamet <pierre@magnusopera.io>
2024-04-23 18:28:26 +0200

    fix branch or tag

--- src/Terrabuild/Helpers/Git.fs
+++ src/Terrabuild/Helpers/Git.fs
@@ -6,9 +6,9 @@ let getHeadCommit (dir: string) =
     | _ -> failwith "Failed to get head commit"
 
 let getBranchOrTag (dir: string) =
-    match Exec.execCaptureOutput dir "git" "rev-parse --abbrev-ref HEAD" with
+    match Exec.execCaptureOutput dir "git" "describe --exact-match --tags" with
     | Exec.Success (output, _) -> output |> String.firstLine
     | _ -> 
-        match Exec.execCaptureOutput dir "git" "git describe --exact-match --tags" with
+        match Exec.execCaptureOutput dir "git" "rev-parse --abbrev-ref HEAD" with
         | Exec.Success (output, _) -> output |> String.firstLine
         | _ -> failwith "Failed to get branch or tag"
