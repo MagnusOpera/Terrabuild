e5e15b42681779151c9b7238e04bc17de974ec71
Pierre Chalamet <pierre@chalamet.net>
2024-05-09 09:44:41 +0200

    fix doc for docker platform

--- src/Terrabuild.Extensions/Docker.fs
+++ src/Terrabuild.Extensions/Docker.fs
@@ -9,6 +9,7 @@ type Docker() =
     /// Build a Dockerfile.
     /// </summary>
     /// <param name="dockerfile" example="&quot;Dockerfile&quot;">Use alternative Dockerfile. Default is Dockerfile.</param>
+    /// <param name="platform" required="false" example="&quot;linux/amd64&quot;">Target platform. Default is host.</param>
     /// <param name="image" required="true" example="&quot;ghcr.io/example/project&quot;">Docker image to build.</param>
     /// <param name="arguments" example="{ configuration: &quot;Release&quot; }">Named arguments to build image (see Dockerfile [ARG](https://docs.docker.com/reference/dockerfile/#arg)).</param> 
     static member build (context: ActionContext) (dockerfile: string option) (platform: string option) (image: string) (arguments: Map<string, string>) =
