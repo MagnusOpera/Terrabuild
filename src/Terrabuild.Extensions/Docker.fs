f16fac9f902e8dbf74df5da4e0f82229d65d0af5
Pierre Chalamet <pierre@chalamet.net>
2024-05-10 08:05:21 +0200

    Revert "add raw args for docker"
    This reverts commit 5f934c4907ba8123a975f408fbcbb762f5d5541d.

--- src/Terrabuild.Extensions/Docker.fs
+++ src/Terrabuild.Extensions/Docker.fs
@@ -12,8 +12,7 @@ type Docker() =
     /// <param name="platform" required="false" example="&quot;linux/amd64&quot;">Target platform. Default is host.</param>
     /// <param name="image" required="true" example="&quot;ghcr.io/example/project&quot;">Docker image to build.</param>
     /// <param name="arguments" example="{ configuration: &quot;Release&quot; }">Named arguments to build image (see Dockerfile [ARG](https://docs.docker.com/reference/dockerfile/#arg)).</param> 
-    /// <param name="rawargs" required="false" example="&quot;--net=host&quot;">Raw arguments for Docker.</param> 
-    static member build (context: ActionContext) (dockerfile: string option) (platform: string option) (image: string) (arguments: Map<string, string>) (rawargs: string option) =
+    static member build (context: ActionContext) (dockerfile: string option) (platform: string option) (image: string) (arguments: Map<string, string>) =
         let dockerfile = dockerfile |> Option.defaultValue "Dockerfile"
         let nodehash = context.NodeHash
 
@@ -22,13 +21,8 @@ type Docker() =
             | Some platform -> $" --platform {platform}"
             | _ -> ""
 
-        let rawargs =
-            match rawargs with
-            | Some rawargs -> $" {rawargs}"
-            | _ -> ""
-
         let args = arguments |> Seq.fold (fun acc kvp -> $"{acc} --build-arg {kvp.Key}=\"{kvp.Value}\"") ""
-        let buildArgs = $"build --file {dockerfile} --tag {image}:{nodehash}{args}{platform}{rawargs} ."
+        let buildArgs = $"build --file {dockerfile} --tag {image}:{nodehash}{args}{platform} ."
 
         if context.CI then
             let pushArgs = $"push {image}:{nodehash}"
