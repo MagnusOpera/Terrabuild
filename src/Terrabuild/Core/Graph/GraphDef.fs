55758f14e970fa5ac5d331cd289e7f3eb0908fd9
Pierre Chalamet <pierre@magnusopera.io>
2024-07-05 16:35:02 +0200

    remove batch label since it's pretty obvious now

--- src/Terrabuild/Core/Graph/GraphDef.fs
+++ src/Terrabuild/Core/Graph/GraphDef.fs
@@ -90,7 +90,7 @@ let render (graph: Graph) =
             let clusterNode = nodes |> List.tryFind (fun node -> node.Id = cluster)
             let isCluster = clusterNode |> Option.isSome
 
-            if isCluster then $"subgraph {cluster}[batch {clusterNode.Value.Target}]"
+            if isCluster then $"subgraph {cluster}[\" \"]"
 
             let offset, nodes =
                 if isCluster then "  ", nodes |> List.filter (fun node -> node.Id <> cluster)
