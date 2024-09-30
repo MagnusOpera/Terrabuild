212271ee0cb8cc4398efa598ace470ed42558fc3
Pierre Chalamet <pierre@chalamet.net>
2024-09-30 09:20:09 +0200

    try workaround for dotnet init bug

--- src/Terrabuild/Core/Cache.fs
+++ src/Terrabuild/Core/Cache.fs
@@ -293,4 +293,9 @@ type Cache(storage: Contracts.IStorage) =
 
         member _.CreateHomeDir nodeHash: string =
             let homeDir = FS.combinePath homeDirectory nodeHash
+            homeDir |> IO.createDirectory
+
+            // long standing bug with .net: https://github.com/dotnet/runtime/issues/36823
+            File.SetUnixFileMode(homeDir, enum<UnixFileMode>(0o777))
+
             homeDir
