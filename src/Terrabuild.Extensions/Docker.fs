deab3a961921fcc8bd9e3bcd4be7984c8e1af586
Pierre Chalamet <pierre@chalamet.net>
2024-11-08 21:26:52 +0100

    Revert "use docker buildx"
    This reverts commit 95c53d8bb3babb60c16a8c3412ffb57494841a12.

--- src/Terrabuild.Extensions/Docker.fs
+++ src/Terrabuild.Extensions/Docker.fs
@@ -11,26 +11,27 @@ type Docker() =
     /// Build a Dockerfile.
     /// </summary>
     /// <param name="dockerfile" example="&quot;Dockerfile&quot;">Use alternative Dockerfile. Default is Dockerfile.</param>
-    /// <param name="platforms" required="false" example="[ &quot;linux/amd64&quot; ]">List of platform to build. Default is host.</param>
+    /// <param name="platform" required="false" example="&quot;linux/amd64&quot;">Target platform. Default is host.</param>
     /// <param name="image" required="true" example="&quot;ghcr.io/example/project&quot;">Docker image to build.</param>
     /// <param name="arguments" example="{ configuration: &quot;Release&quot; }">Named arguments to build image (see Dockerfile [ARG](https://docs.docker.com/reference/dockerfile/#arg)).</param> 
-    static member build (context: ActionContext) (dockerfile: string option) (platforms: string list option) (image: string) (arguments: Map<string, string>) =
+    static member build (context: ActionContext) (dockerfile: string option) (platform: string option) (image: string) (arguments: Map<string, string>) =
         let dockerfile = dockerfile |> Option.defaultValue "Dockerfile"
 
-        let platformArgs =
-            match platforms with
-            | None -> ""
-            | Some platforms ->
-                platforms
-                |> Seq.fold (fun acc platform -> $"{acc} --platform {platform}") ""
+        let platform =
+            match platform with
+            | Some platform -> $" --platform {platform}"
+            | _ -> ""
 
         let args = arguments |> Seq.fold (fun acc kvp -> $"{acc} --build-arg {kvp.Key}=\"{kvp.Value}\"") ""
 
         let ops = 
             [
-                let pushArgs = if context.CI then " --push" else ""
-                let buildArgs = $"buildx build --file {dockerfile} --tag {image}:{context.Hash}{args}{platformArgs}{pushArgs} ."
-                shellOp "docker" buildArgs
+                let buildArgs = $"build --file {dockerfile} --tag {image}:{context.Hash}{args}{platform} ."
+                if context.CI then
+                    shellOp "docker" buildArgs
+                    shellOp "docker" $"push {image}:{context.Hash}"
+                else
+                    shellOp "docker" buildArgs
             ]
 
         let cacheability =
