31f1e4a34b20fb13fe742a3331a871a0dd9eeef1
Pierre Chalamet <pierre@magnusopera.io>
2025-01-05 10:08:36 +0100

    integration test with custom script (#80)
    Check regression with previous versions.

--- tests/simple/scripts/npm.fsx
+++ tests/simple/scripts/npm.fsx
@@ -1,35 +1,115 @@
 #if !TERRABUILD_SCRIPT
-#r "../../../src/Terrabuild/bin/Debug/net8.0/Terrabuild.Extensibility.dll"
+#r "../../../src/Terrabuild/bin/Debug/net9.0/Terrabuild.Extensibility.dll"
 #endif
 
 open Terrabuild.Extensibility
 
 
-let __defaults__() =
+module String =
+    open System.Text.RegularExpressions
+
+    let (|Regex|_|) pattern input =
+        let m = Regex.Match(input, pattern)
+        if m.Success then Some(List.tail [ for g in m.Groups -> g.Value ])
+        else None
+
+
+module NpmHelpers =
+    open System.Text.Json.Serialization
+    open System.Text.Json
+    open System.Collections.Generic
+
+    [<CLIMutable>]
+    type Package = {
+        [<JsonPropertyName("dependencies")>]
+        Dependencies: Dictionary<string, string>
+
+        [<JsonPropertyName("devDependencies")>]
+        DevDependencies: Dictionary<string, string>
+    }
+
+    let findProjectFile (directory: string) =
+        let projects =
+            System.IO.Directory.EnumerateFiles(directory, "package.json")
+            |> List.ofSeq
+        match projects with
+        | [ project ] -> project
+        | [] -> failwith "No project found"
+        | _ -> failwith "Multiple projects found"
+
+    let findDependencies (projectFile: string) =
+        let json = System.IO.File.ReadAllText projectFile
+        let package = JsonSerializer.Deserialize<Package>(json)
+
+        let dependencies = seq {
+            if package.Dependencies|> isNull |> not then
+                for (KeyValue(_, value)) in package.Dependencies do
+                    match value with
+                    | String.Regex "^file:(.*)$" [project] -> yield project
+                    | _ -> ()
+
+            if package.DevDependencies|> isNull |> not then
+                for (KeyValue(_, value)) in package.DevDependencies do
+                    match value with
+                    | String.Regex "^file:(.*)$" [project] -> yield project
+                    | _ -> ()
+        }
+        Set dependencies
+
+
+
+
+
+let __defaults__ (context: ExtensionContext) =
+    let projectFile = NpmHelpers.findProjectFile context.Directory
+    let dependencies = projectFile |> NpmHelpers.findDependencies 
     let projectInfo = 
-        { ProjectInfo.Default
-            with Ignores = Set [ "node_modules/" ]
-                 Outputs = Set [ "dist/" ] }
+        { ProjectInfo.Default with
+            Ignores = Set [ "node_modules/" ]
+            Outputs = Set [ "dist/" ]
+            Dependencies = dependencies }
     projectInfo
 
-let install () =
-    let ops = All [ shellOp "npm" "ci" ]
-    execRequest Cacheability.Always [] ops
 
-let build (arguments: string option) =
+let __dispatch__ (context: ActionContext) (arguments: string option) =
+    let cmd = context.Command
+    let arguments = arguments |> Option.defaultValue ""
+
+    let ops = [
+        shellOp "npm" "ci"
+        shellOp "npm" $"run {cmd} -- {arguments}"   
+    ]
+    execRequest Cacheability.Always ops
+
+
+let install (context: ActionContext) =
+    let ops = [ shellOp "npm" "ci" ]
+    execRequest Cacheability.Always ops
+
+
+let build (context: ActionContext) (arguments: string option) =
     let args = arguments |> Option.defaultValue ""
 
-    let ops = All [
+    let ops = [
         shellOp "npm" "ci"
-        shellOp "npm" $"run build -- {args}"
+        shellOp "npm" $"run build -- {args}"   
     ]
-    execRequest Cacheability.Always [] ops
+    execRequest Cacheability.Always ops
 
-let test (arguments: string option) =
+
+let test (context: ActionContext) (arguments: string option) =
     let args = arguments |> Option.defaultValue ""
 
-    let ops = All [
+    let ops = [
         shellOp "npm" "ci"
-        shellOp "npm" $"run test -- {args}"
+        shellOp "npm" $"run test -- {args}"   
     ]
-    execRequest Cacheability.Always [] ops
+    execRequest Cacheability.Always ops
+
+let run (context: ActionContext) (command: string) (arguments: string option) =
+    let args = arguments |> Option.defaultValue ""
+
+    let ops = [
+        shellOp "npm" $"run {command} -- {args}"
+    ]
+    execRequest Cacheability.Always ops
