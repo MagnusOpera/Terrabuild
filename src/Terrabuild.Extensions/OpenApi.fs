99c1af467659aaf90ac0c4005c224f6cb3ca8a5d
Pierre Chalamet <pierre@chalamet.net>
2025-01-19 22:16:36 +0100

    fix docs

--- src/Terrabuild.Extensions/OpenApi.fs
+++ src/Terrabuild.Extensions/OpenApi.fs
@@ -2,12 +2,12 @@ namespace Terrabuild.Extensions
 open Terrabuild.Extensibility
 
 /// <summary>
-/// Provides support for `openapitools`.
+/// Provides support for `OpenAPI Generator`.
 /// </summary>
 type OpenApi() =
 
     /// <summary>
-    /// Generate api.
+    /// Generate using `openapi-generator-cli`.
     /// </summary>
     static member generate (context: ActionContext) (generator: string) (input: string) (output: string) =
         let args = $"generate -i {input} -g {generator} -o {output}"
