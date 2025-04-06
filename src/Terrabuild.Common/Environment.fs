729888699fb5c9a3bbf8fd55a5589aac3c2fff41
Pierre Chalamet <pierre@magnusopera.io>
2025-04-06 14:35:28 +0200

    Use env variable content to detect configuration changes (#139)

--- src/Terrabuild.Common/Environment.fs
+++ src/Terrabuild.Common/Environment.fs
@@ -1,3 +1,6 @@
 module Environment
 
 let IsLinux = System.Runtime.InteropServices.RuntimeInformation.IsOSPlatform(System.Runtime.InteropServices.OSPlatform.Linux)
+
+let getEnvVar (name: string) =
+    System.Environment.GetEnvironmentVariable(name) |> Option.ofObj
