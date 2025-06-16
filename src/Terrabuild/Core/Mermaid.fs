4a18631c435f721843fe65e59d659d4b4bd734e8
Pierre Chalamet <pierre@magnusopera.io>
2025-06-16 07:31:21 +0200

    Revert "revert pastel colors"
    This reverts commit 660d351bdff1f9e8796c6a0add1cff9e50bc57bb.

--- src/Terrabuild/Core/Mermaid.fs
+++ src/Terrabuild/Core/Mermaid.fs
@@ -9,6 +9,20 @@ type GetStatus = Node -> string
 type GetOrigin = Node -> Build.TaskRequest option
 
 
+let scaleToPastel (value: int) : int =
+    // Map from [0, 255] to [160, 240]
+    160 + (value * (240 - 160) / 255)
+
+let pastelColorFromHash (md5hash: string) : string =
+    if md5hash.Length < 6 then
+        invalidArg "md5hash" "MD5 hash string must be at least 6 characters long"
+
+    let r = Convert.ToInt32(md5hash.Substring(0, 2), 16) |> scaleToPastel
+    let g = Convert.ToInt32(md5hash.Substring(2, 2), 16) |> scaleToPastel
+    let b = Convert.ToInt32(md5hash.Substring(4, 2), 16) |> scaleToPastel
+
+    $"#{r:X2}{g:X2}{b:X2}"
+
 let render (getStatus: GetStatus option) (getOrigin: GetOrigin option) (graph: Graph) =
     let mermaid = [
         "flowchart TD"
@@ -41,6 +55,8 @@ let render (getStatus: GetStatus option) (getOrigin: GetOrigin option) (graph: G
             | Some Build.TaskRequest.Build -> $"class {node.Id} build"
             | Some Build.TaskRequest.Restore -> $"class {node.Id} restore"
             | _ -> $"class {node.Id} ignore"
+            let color = node.ProjectDir |> Hash.md5 |> pastelColorFromHash 
+            $"style {node.Id} fill:{color}"
     ]
 
     mermaid
