a383502099c36d37a038d456ae1eee3909eb9325
Pierre Chalamet <pierre@magnusopera.io>
2025-06-20 10:15:27 +0200

    add additional properties support

--- src/Terrabuild.Extensions/OpenApi.fs
+++ src/Terrabuild.Extensions/OpenApi.fs
@@ -11,8 +11,14 @@ type OpenApi() =
     /// <summary>
     /// Generate using `openapi-generator-cli`.
     /// </summary>
-    static member generate (context: ActionContext) (generator: string) (input: string) (output: string) =
-        let args = $"generate -i {input} -g {generator} -o {output}"
+    static member generate (context: ActionContext) (generator: string) (input: string) (output: string) (properties: Map<string, string>) =
+        let props =
+            if properties |> Map.isEmpty then ""
+            else
+                let args = properties |> Seq.map (fun kvp -> $"{kvp.Key}={kvp.Value}") |> String.concat ","
+                $"--additional-properties={args}"
+
+        let args = $"generate -i {input} -g {generator} -o {output} {props}"
 
         let ops = [
             shellOp("docker-entrypoint.sh", args)
