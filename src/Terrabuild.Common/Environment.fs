08f197762f3036376106e2c224dd3a3a6e0b52c7
Pierre Chalamet <pierre@magnusopera.io>
2025-06-21 21:08:49 +0200

    feat: use regex for describing container vars (#191)

--- src/Terrabuild.Common/Environment.fs
+++ src/Terrabuild.Common/Environment.fs
@@ -1,8 +1,14 @@
 module Environment
-
-let IsLinux = System.Runtime.InteropServices.RuntimeInformation.IsOSPlatform(System.Runtime.InteropServices.OSPlatform.Linux)
+open System
+open System.Collections
 
 let envVar (varName: string) =
-    varName |> System.Environment.GetEnvironmentVariable |> Option.ofObj
+    varName |> Environment.GetEnvironmentVariable |> Option.ofObj
 
 let currentDir() = System.Environment.CurrentDirectory
+
+let envVars() =
+    Environment.GetEnvironmentVariables()
+    |> Seq.cast<DictionaryEntry>
+    |> Seq.map (fun entry -> $"{entry.Key}", $"{entry.Value}")
+    |> Map.ofSeq
